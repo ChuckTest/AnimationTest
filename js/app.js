b9b1be322af74b75851a6793eee0b502746435f8
Chuck Lu <chuck.lu@qq.com>
2020-09-23 16:23:09 +0800

    auto stop interval

--- js/app.js
+++ js/app.js
@@ -1,15 +1,32 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+    getBodySize();
     myFunction();
 });
 
+var refreshIntervalId;
 function myFunction() {
-    setInterval(moveFunction, 1000);
+    refreshIntervalId = setInterval(moveFunction, 1000);
+}
+
+
+var bodyHeight;
+var bodyWidth;
+function getBodySize() {
+    var element = document.getElementsByTagName("BODY")[0];
+    var positionInfo = element.getBoundingClientRect();
+    bodyHeight = positionInfo.height;
+    bodyWidth = positionInfo.width;
+    console.log(bodyHeight);
+    console.log(bodyWidth);
 }
 
 var margin = 0;
 function moveFunction() {
     var element = document.getElementById("test");
-    margin = margin + 1;
+    margin = margin + 10;
+    if (margin > bodyHeight || margin > bodyWidth) {
+        clearInterval(refreshIntervalId);
+    }
     element.style.marginLeft = margin + "px";
     element.style.marginTop = margin + "px";
 }
